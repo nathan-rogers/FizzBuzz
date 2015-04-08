55597be0cd5c5cc51187740bca6f58430722f795
Nathan Rogers <nathan.rogers.d@gmail.com>
2015-04-08 14:26:03 -0600

    Created FizzBuzz function
    Checks for divisibility between 3 and 5, if so prints either fizz or
    buzz, or fizzbuzz if both conditions are met

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countTo = 0;
+
+            Console.WriteLine("Let's play Fizz Buzz!" );
+            Console.WriteLine("What number would you like to count to?");
+            countTo = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= countTo; i++)
+            {
+                Console.WriteLine(FizzBuzz(i));
+            }
+            Console.ReadKey();
+        }
+        public static string FizzBuzz(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz!";
+            }
+            else if (number % 3 == 0 || number % 5 == 0)
+            {
+                if (number % 3 == 0)
+                {
+                    return "Fizz!";
+                }
+                else
+                {
+                    return "Buzz!";
+                }
+            }
+            else
+            {
+                return number.ToString();
+            }
+        }
+    }
+}
