ae2f90372b65a7a9c849626844a9fa4155de4c3a
Nathan Rogers <nathan.rogers.d@gmail.com>
2015-04-08 14:50:55 -0600

    declared variable in other location

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -10,25 +10,41 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            int countTo = 0;
+            
 
             Console.WriteLine("Let's play Fizz Buzz!" );
+
+            //asks user for input
             Console.WriteLine("What number would you like to count to?");
-            countTo = int.Parse(Console.ReadLine());
+
+            //variable stores user input
+            int countTo = int.Parse(Console.ReadLine());
+
+            //loops to number selected by user
             for (int i = 0; i <= countTo; i++)
             {
                 Console.WriteLine(FizzBuzz(i));
             }
             Console.ReadKey();
         }
+        /// <summary>
+        /// Creates fizzbuzz game. Automatically prints, fizz, buzz, or fizzbuzz
+        /// Depending on conditional requiremets, 
+        /// Divisible by 3, 5, or 3 and 5
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
         public static string FizzBuzz(int number)
         {
+            //if both conditions are met
             if (number % 3 == 0 && number % 5 == 0)
             {
                 return "FizzBuzz!";
             }
+            //if either condition is met
             else if (number % 3 == 0 || number % 5 == 0)
             {
+                //checks for divisibility between either 3 or 5
                 if (number % 3 == 0)
                 {
                     return "Fizz!";
@@ -38,6 +54,8 @@ namespace FizzBuzz
                     return "Buzz!";
                 }
             }
+            //if none of the conditions are met, returns number
+            //converts number to string
             else
             {
                 return number.ToString();
